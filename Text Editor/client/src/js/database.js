27f542f253f292d024fccba4779221645a37862b
Zavier Jackson <158118588+Zjack23@users.noreply.github.com>
2024-07-18 14:01:45 -0400

    Add files via upload

---
+++ Text Editor/client/src/js/database.js
@@ -0,0 +1,19 @@
+// Add logic to add some content to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+// Add logic for getting all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  return result?.content;
+};
